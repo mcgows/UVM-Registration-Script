1fde53988a766249bb8f649506ee023029407deb
Jack McGowan <jackmcgowan12@gmail.com>
2022-04-06 13:44:45 -0400

    Lower storage requirements by removing blank space

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import config
-
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait, Select
 
 ### Initialize the WebDriver
-
 driver = webdriver.Safari()
 driver.maximize_window()
 
 ### Go to the desired Starting Address
 driver.get(config.URL)
-
 
 ### Automate Login
 username_box = WebDriverWait(driver, 10).until(
@@ -24,19 +19,16 @@ username_box = WebDriverWait(driver, 10).until(
         )
 username_box.send_keys(config.NETID)
 
-
 password_box = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "PIN"))
         )
 password_box.send_keys(config.NETID_PASSWORD + Keys.RETURN)
-
 
 #### Select 'Registration'
 element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Registration"))
         )
 element.click()
-time.sleep(0.2)
 
 #### Select 'Register for Courses'
 element = WebDriverWait(driver, 10).until(
@@ -55,13 +47,11 @@ for o in select.options:
     if o.text is config.TERM:
         o.click()
 
-
 ### Submit the term request
 element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.TAG_NAME, "input"))
         )
 element.click()
-
 
 ### Select all the ID Boxes
 crn_boxes = WebDriverWait(driver, 10).until(
